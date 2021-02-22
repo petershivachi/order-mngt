589423d3377570e674a6a651b5f012003f28234c
petershivachi <shitsukanepeter@gmailc.om>
2021-02-22 22:42:34 +0300

    route files with express

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running...'))
 
+//Define Routes
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/products', require('./routes/api/products'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
