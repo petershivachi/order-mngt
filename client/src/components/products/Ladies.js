89cd9bddbe441722e7ee2aeb35d442f4a782b85f
petershivachi <shitsukanepeter@gmailc.om>
2021-03-03 17:19:57 +0300

    created the ladies, accesorries, kids, men and the dashboard components

---
+++ client/src/components/products/Ladies.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Ladies = props => {
+  return (
+    <section id="trending">
+      <div className="trending py-1">
+        <div className="container grid grid-3">
+          <div className="card flex">
+            <img src="./img/ladies/pic1.jpg" alt=""/>
+          <div className="price-wrapper">
+            <h2>Zanzea Women's Blouse O-Neck</h2>
+            <h3>Ksh. 779</h3>
+            <p>Leopard High? Low Hem Shirt.</p>
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+      </div>
+      <div className="card flex">
+        <img src="./img/ladies/pic2.jpg" alt=""/>
+      <div className="price-wrapper">
+        <h2>Fashion Vogue Fashion Unique</h2>
+        <h3>Ksh. 513</h3>
+        <p>Women's Girls Novelty Big Bow Tie.</p>
+        <button className="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic3.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Fashion women dress Fashion</h2>
+      <h3>Ksh. 599</h3>
+      <p>Contracted Floral Emboiddery Dress.</p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic4.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Generic SALE! Women Off-Shoulder</h2>
+      <h3>Ksh. 2000</h3>
+      <p>Dress vlouses Irregular Dresses.</p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic5.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Fashion Women elegant Sandals</h2>
+      <h3>Ksh. 369</h3>
+      <p>Wedge Purple.</p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic6.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Fashion Women Stretch Skinny</h2>
+      <h3>Ksh. 599</h3>
+      <p>Pants Blue.</p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic7.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Aomeo Women Party Casual Dress</h2>
+      <h3>Ksh. 1600</h3>
+      <p>Pleated Sexy Body Long Brown</p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic8.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Fashion Women Knitted Sweater</h2>
+      <h3>Ksh. 1600</h3>
+      <p>Round Necklong Sleeve.</p>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+    </div>
+    <div className="card flex">
+      <img src="./img/ladies/pic9.jpg" alt=""/>
+    <div className="price-wrapper">
+      <h2>Zanzea Women Pure Color</h2>
+      <h3>Ksh. 1305</h3>
+      <p>Fashion women's Dress Fashion.</p>
+      <button className="btn btn-primary">Buy Now</button>ra
+    </div>
+    </div>
+      </div>
+    </div>
+  </section>
+
+    
+  )
+}
+
+Ladies.propTypes = {
+  
+}
+
+export default Ladies;
